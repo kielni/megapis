e88602c9790713e6247177af7ac02e59c80e7047
Kimberly Nicholls <kimberly@gridium.com>
2015-07-19 20:50:49 -0700

    command line config file option

--- server/run_worker.js
+++ server/run_worker.js
@@ -5,13 +5,16 @@ var megapisUtil = require("./util"),
     CronJob = require("cron").CronJob,  
     jf = require("jsonfile"),
     log4js = require("log4js"),
-    log = log4js.getLogger("megapis-server");
+    log = log4js.getLogger("megapis-server"),
+    argv = require("minimist")(process.argv.slice(2));
 
 /*
     run a single worker by id
 */
-var config = megapisUtil.loadGlobalConfig("config/global.json");
+
 var id = process.argv[2];
+var configFile = argv.c || "config/global.json";
+var config = megapisUtil.loadGlobalConfig(configFile);
 var workerObj = config.workers[id];
 if (!workerObj) {
     log.error("can't find worker "+id);
