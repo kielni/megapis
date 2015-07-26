e44d0356fb65010cd6011e0000ce8547dca23cfc
Kimberly Nicholls <kimberly@gridium.com>
2015-07-25 17:50:57 -0700

    exclude ASINs found in `config.exclude` key; get author

--- workers/prime_books/README.md
+++ workers/prime_books/README.md
@@ -2,4 +2,17 @@
 
 Get Kindle lending library eligible books from genre page(s) in `config.urls`.
 Go to each book's product page to get its description and save to `config.output`.
+If `config.exclude` is set, get exclude any matching URLs.
 
+## sample object
+
+
+    {
+        "asin":"B00SM2U12A",
+        "title":"Crazy Love",
+        "author":"Casey L. Bond",
+        "url":"http://www.amazon.com/Crazy-Love-Casey-L-Bond-ebook/dp/B00SM2U12A",
+        "description":"Shelby Case avoids town and all the people in it. She can't stand the whispers behind covered lips, the judgmental stares. People say she's crazy, that she lost her mind when she lost her husband. \r\rThe Second U.S. Civil War has left widows and widowers scattered across the land. Shelby's just learned to cope. She's taught herself the fine art of self-preservation along with the skills to survive. \r\rWhen the figment of her imagination, the man who's haunted her days and nights for the past two y...",
+        "tag":"Crazy Love, Casey L. Bond, Anna Coy - Amazon.com",
+        "source":"primeBooks"
+    }
--- workers/prime_books/index.js
+++ workers/prime_books/index.js
@@ -23,58 +23,81 @@ PrimeBooksWorker.prototype.getConfigKeys = function() {
 };
 
 PrimeBooksWorker.prototype.run = function(callback) {
-    var books = [];
-    var byUrl = {};
     var self = this;
-    async.forEach(this.config.urls, function(url, callback) {
-        request(url, function(err, response, body) {
-            if (err) throw err;
-            var $ = cheerio.load(body);
-            // get books
-            $(".s-access-detail-page").each(function() {
-                var title = $(this).attr("title");
-                var url = $(this).attr("href");
-                //log.debug("title="+title+" url="+url);
-                byUrl[url] = {
-                    title: title,
-                    url: url
-                };
+    async.waterfall([
+        function getExclude(next) {
+            if (self.config.exclude) {
+                self.get(self.config.exclude, function(err, values) {
+                    log.debug("found "+values.length+" ASINs to exclude");
+                    next(null, values);
+                });
+            } else {
+                next(null, []);
+            }
+        },
+        function getBooks(exclude, next) {
+            var byUrl = {};
+            async.forEach(self.config.urls, function(url, forEachCallback) {
+                request(url, function(err, response, body) {
+                    if (err) throw err;
+                    var $ = cheerio.load(body);
+                    // get books
+                    $(".s-result-item").each(function() {
+                        var asin = $(this).attr("data-asin");
+                        var item = $(this).find(".s-access-detail-page");
+                        var title = $(item).attr("title");
+                        var author = $(this).find(".a-row .a-row a").eq(2).text();
+                        var url = $(item).attr("href");
+                        //log.debug("exclude "+url+"="+_.includes(exclude, url));
+                        if (!_.includes(exclude, asin)) {
+                            byUrl[url] = {
+                                asin: asin,
+                                title: title,
+                                author: author,
+                                url: url
+                            };
+                        }
+                    });
+                    forEachCallback();
+                });
+            }, function(err) {
+                next(err, byUrl);
             });
-            callback();
-        });
-    }, function(err) {
-        var i = 0;
-        var total= _.keys(byUrl).length;
-        log.info("found "+total+" books");
-        async.forEach(_.keys(byUrl), function(url, callback) {
-            // get synopsis from detail page link
-            request(url, function(err, response, body) {
-                if (err) throw err;
-                log.debug("url=", url);
-                var $ = cheerio.load(body);
-                var book = byUrl[url];
-                book.url = $("link[rel='canonical']").attr("href");
-                book.description = $("#bookDescription_feature_div noscript").text().trim();
-                if (book.description.length > 500) {
-                    book.description = book.description.substring(0, 500)+"...";
-                }
-                log.debug("url="+url+"\n"+book.description+"\n\n");
-                var tag = $("meta[name='title']").attr("content");
-                tag = tag.replace("Amazon.com: ", "");
-                tag = tag.replace(": Kindle Store", "");
-                book.tag = tag;
-                book.source = self.config.id;
-                books.push(book);
-                i++;
-                if (i%10 === 0) {
-                    log.debug("loaded "+i+"/"+total+" book pages");
-                }
-                callback();
+        },
+        function getDetails(byUrl, next) {
+            var books = [];
+            var i = 0;
+            var total= _.keys(byUrl).length;
+            log.info("found "+total+" books");
+            async.forEach(_.keys(byUrl), function(url, forEachCallback) {
+                // get synopsis from detail page link
+                request(url, function(err, response, body) {
+                    if (err) throw err;
+                    var $ = cheerio.load(body);
+                    var book = byUrl[url];
+                    book.url = $("link[rel='canonical']").attr("href");
+                    book.description = $("#bookDescription_feature_div noscript").text().trim();
+                    if (book.description.length > 500) {
+                        book.description = book.description.substring(0, 500)+"...";
+                    }
+                    var tag = $("meta[name='title']").attr("content");
+                    tag = tag.replace("Amazon.com: ", "");
+                    tag = tag.replace(": Kindle Store", "");
+                    book.tag = tag;
+                    book.source = self.config.id;
+                    // TODO: author?
+                    books.push(book);
+                    i++;
+                    if (i%10 === 0) {
+                        log.debug("loaded "+i+"/"+total+" book pages");
+                    }
+                    forEachCallback();
+                });
+            }, function(err) {
+                next(err, books);
             });
-        }, function(err) {
-            self.save(books, callback);
-        });
+        }
+    ], function(err, books) {
+        self.save(books, callback);
     });
 };
-
-
